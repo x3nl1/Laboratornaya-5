15e425db1330aefdd2c74e0e6d2b901be9485083
x3nl1 <stepanagapov2005@gmail.com>
2025-12-02 21:46:03 +0300

    completed task 1

---
+++ task 1(number 1 easy).py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Привет")
+
+label = tk.Label(window, text="Нажми кнопку")
+label.pack(pady=20)
+
+button = tk.Button(window, text="Привет", 
+                   command=lambda: label.config(text="Привет, мир!"))
+button.pack(pady=10)
+
+window.mainloop()
