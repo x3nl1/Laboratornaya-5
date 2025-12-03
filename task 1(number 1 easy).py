536c1244e6fefbbd34719a08c38003c7a0e2a727
x3nl1 <stepanagapov2005@gmail.com>
2025-12-03 11:57:17 +0300

    Update task 1(number 1 easy).py

--- task 1(number 1 easy).py
+++ task 1(number 1 easy).py
@@ -7,7 +7,7 @@ label = tk.Label(window, text="Нажми кнопку")
 label.pack(pady=20)
 
 button = tk.Button(window, text="Привет", 
-                   command=lambda: label.config(text="Привет, мир!"))
+command=lambda: label.config(text="Привет, мир!"))
 button.pack(pady=10)
 
 window.mainloop()
